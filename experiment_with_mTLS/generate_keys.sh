a4e71bf1327e6879838f0b9d04a390dc8eb50c2f
Mauro de Carvalho <mauro.dcg@gmail.com>
2024-04-20 12:48:10 -0300

    feat: adding esp32 certificate and key generation commands

--- experiment_with_mTLS/generate_keys.sh
+++ experiment_with_mTLS/generate_keys.sh
@@ -2,13 +2,21 @@
 sudo apt-get update
 sudo apt-get install openssl
 
+# Generate keys for the CA
 openssl genrsa -des3 -out ca.key 2048;
 openssl req -new -x509 -days 1826 -key ca.key -out ca.crt;
 
+# Generate keys for the broker
 openssl genrsa -out broker.key 2048;
 openssl req -new -out broker.csr -key broker.key;
 openssl x509 -req -in broker.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out broker.crt -days 360;
 
+# Generate keys for the application
 openssl genrsa -out application.key 2048;
 openssl req -new -out application.csr -key application.key;
 openssl x509 -req -in application.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out application.crt -days 360
+
+# Generate keys for the ESP32
+openssl genrsa -out esp.key 2048;
+openssl req -new -out esp.csr -key esp.key;
+openssl x509 -req -in esp.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out esp.crt -days 360
