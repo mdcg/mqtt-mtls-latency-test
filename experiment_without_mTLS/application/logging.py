53ff3a852cfc640208d9dcd65d30f0058e948553
Mauro de Carvalho <mauro.dcg@gmail.com>
2024-04-21 15:08:01 -0300

    feat: saving logs into files

--- experiment_without_mTLS/application/logging.py
+++ experiment_without_mTLS/application/logging.py
@@ -2,11 +2,17 @@ import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-handler = logging.StreamHandler()
-handler.setFormatter(
-    logging.Formatter(
-        fmt="[without-mTLS] %(asctime)s - %(levelname)s : %(message)s",
-        datefmt="%d/%m/%Y %I:%M:%S %p",
-    )
+
+formatter = logging.Formatter(
+    fmt="[without-mTLS] %(asctime)s - %(levelname)s : %(message)s",
+    datefmt="%d/%m/%Y %I:%M:%S %p",
 )
+
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler('without_mTLS.log')
+file_handler.setFormatter(formatter)
+
 logger.addHandler(handler)
+logger.addHandler(file_handler)
